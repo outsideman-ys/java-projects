82587624f79027690cb302afa68f224b96234b5b
bob <y.smirnov01@gmail.com>
2023-10-04 13:11:38 +0700

    ver1.1

--- StepTracker/App.java
+++ StepTracker/App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         StepTracker stepTracker = new StepTracker();
         Menu.printMenu(stepTracker);
+
     }
 }
